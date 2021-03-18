dcc1f2c0bba3f03e7381508a35a6a396f3433a10
minhdang741 <minhdang741@github.com>
2021-03-18 18:22:49 +0700

    commited

---
+++ feroxsystem/feroxsystemapp/connect.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+
+# Connect to local MongoDB
+class Connect(object):
+    @staticmethod
+    def connection():
+        client = MongoClient("mongodb://localhost:27017/")
+        customer = client.customerdata.customer
+        return customer
--- feroxsystem/feroxsystemapp/views.py
+++ feroxsystem/feroxsystemapp/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .connect import Connect
+import json
 
 # Create your views here.
 
+
+"""
 def index(request):
     return render(request, 'pages/Signin.html')
+"""
+
+
+def index(request):
+    customer = Connect.connection()
+    file = open('/home/dang/Documents/customer/customer (another copy).json', 'r')
+    """
+    with open('/home/dang/Documents/customer/customer (copy).json') as f:
+        data = json.loads(f)
+    """
+    data = json.load(file)
+    # print(data)
+    customer.insert_many(data)
+    file.close()
+    return HttpResponse("Done")
